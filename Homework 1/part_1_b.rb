b0133f264ff1c7551a75ba75a0dd94226376d998
❥ giulia <giuliamferreira01@gmail.com>
2024-10-29 19:08:19 -0300

    Update part_1_b.rb

--- Homework 1/part_1_b.rb
+++ Homework 1/part_1_b.rb
@@ -1,8 +1,8 @@
 # Write a method that determines whether a given word or phrase is a palindrome, that is, it reads the same backwards as forwards, ignoring case, punctuation, and nonword characters.
 
 def palindrome?(string)
-  normalized_string = string.downcase.gsub(/\W/, '')
-  puts normalized_string == normalized_string.reverse
+  normalized_string = string.downcase.gsub(/\W/, '')  # Remove palavras não alfanuméricas e converte para minúsculas
+  puts normalized_string == normalized_string.reverse # Verifica se a string é igual a ela mesma invertida e retorna o resultado
 end
 
 # Casos de teste
