1abacbb986d113caea9b8b6bf2cf5c67f9fc5f40
❥ giulia <giuliamferreira01@gmail.com>
2024-10-29 18:17:16 -0300

    Add files via upload

---
+++ Homework 1/palindrome.rb
@@ -0,0 +1,15 @@
+# https://github.com/giuliamf
+# Giulia Moura, 200018795
+
+# Write a method that determines whether a given word or phrase is a palindrome, that is, it reads the same backwards
+# as forwards, ignoring case, punctuation, and nonword characters.
+
+def palindrome?(string)
+  normalized_string = string.downcase.gsub(/\W/, '')
+  puts normalized_string == normalized_string.reverse
+end
+
+# Casos de teste
+palindrome?("A man, a plan, a canal -- Panama")
+palindrome?("Madam, I'm Adam!")
+palindrome?("Abracadabra")
