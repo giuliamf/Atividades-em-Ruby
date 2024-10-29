b75310564f6c3f87c503dfa5d2ccd58279e65cd1
❥ giulia <giuliamferreira01@gmail.com>
2024-10-29 20:02:21 -0300

    Update part_3.rb

--- Homework 1/part_3.rb
+++ Homework 1/part_3.rb
@@ -1,6 +1,5 @@
 # Given an array of strings, write a method that groups them into anagram groups and returns the array of groups.
-# Case doesn't matter in classifying string as anagrams (but case should be preserved in the output), and the order of
-# the anagrams in the groups doesn't matter
+# Case doesn't matter in classifying string as anagrams (but case should be preserved in the output), and the order of the anagrams in the groups doesn't matter
 
 def combine_anagrams(words)
   anagrams = Hash.new
