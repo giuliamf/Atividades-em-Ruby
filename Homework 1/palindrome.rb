b9b72238e604c7a405167247b00dc0fe75b0db2a
❥ giulia <giuliamferreira01@gmail.com>
2024-10-29 18:41:10 -0300

    Update palindrome.rb

--- Homework 1/palindrome.rb
+++ Homework 1/palindrome.rb
@@ -1,6 +1,3 @@
-# https://github.com/giuliamf
-# Giulia Moura, 200018795
-
 # Write a method that determines whether a given word or phrase is a palindrome, that is, it reads the same backwards as forwards, ignoring case, punctuation, and nonword characters.
 
 def palindrome?(string)
