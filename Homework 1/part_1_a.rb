26d25a72f1abaad90552359d32b04c4ef0f9bd6f
❥ giulia <giuliamferreira01@gmail.com>
2024-10-29 19:32:13 -0300

    Update part_1_a.rb

--- Homework 1/part_1_a.rb
+++ Homework 1/part_1_a.rb
@@ -9,4 +9,6 @@ end
 
 # Casos de teste
 count_words("A man, a plan, a canal -- Panama")
+# => {"a"=>3, "man"=>1, "plan"=>1, "canal"=>1, "panama"=>1}
 count_words("Doo bee doo bee doo")
+# => {"doo"=>3, "bee"=>2}
